ba7eab4acb4e9eae3711e9021b4589fcad6dfc8e
HsiaoHungWang <wang@ispan.com.tw>
2024-08-08 10:46:47 +0800

    回傳JSON資料

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,13 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AjaxDemo.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AjaxDemo.Controllers
 {
     public class ApiController : Controller
     {
+        private readonly mydbContext _context;
+
+        public ApiController(mydbContext context)
+        {            
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             string content = "<h2>Ajax, 您好</h2>";
             return Content(content,"text/plain",System.Text.Encoding.UTF8);
         }
+
+        public IActionResult Cities() {
+           var cities = _context.Addresses.Select(a=>a.City).Distinct();
+           return Json(cities);
+        }
+
     }
 }

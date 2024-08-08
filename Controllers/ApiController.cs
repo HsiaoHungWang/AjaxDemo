308091754a50b601d979a4bbd676f52eaf73e6cc
HsiaoHungWang <wang@ispan.com.tw>
2024-08-08 10:15:43 +0800

    Content() 方法

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxDemo.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            string content = "<h2>Ajax, 您好</h2>";
+            return Content(content,"text/plain",System.Text.Encoding.UTF8);
+        }
+    }
+}
